151381dd589a410b58feff097f198557cd13d38d
RIDA LADIB <ridlad7@gmail.com>
2025-04-16 16:30:58 +0000

    feat: add the number_guess.sh file

---
+++ number_guess.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+# Generate random number between 1-1000
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+
+# Get username
+echo "Enter your username:"
+read USERNAME
+
+# Check if user exists
+USER_INFO=$($PSQL "SELECT username, games_played, best_game FROM users WHERE username='$USERNAME'")
+
+if [[ -z $USER_INFO ]]; then
+  # New user
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  INSERT_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+else
+  # Existing user
+  IFS='|' read -r USERNAME GAMES_PLAYED BEST_GAME <<< "$USER_INFO"
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+# Start game
+GUESS_COUNT=0
+CORRECT=false
+
+echo "Guess the secret number between 1 and 1000:"
+
+while ! $CORRECT; do
+  read GUESS
+
+  # Validate input is integer
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+
+  ((GUESS_COUNT++))
+
+  if [[ $GUESS -eq $SECRET_NUMBER ]]; then
+    CORRECT=true
+    echo "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job!"
+
+    # Update user stats
+    if [[ -z $USER_INFO ]]; then
+      # First game for new user
+      UPDATE_RESULT=$($PSQL "UPDATE users SET games_played=1, best_game=$GUESS_COUNT WHERE username='$USERNAME'")
+    else
+      # Existing user
+      NEW_GAMES_PLAYED=$((GAMES_PLAYED + 1))
+      
+      # Check if this is their best game
+      if [[ $GUESS_COUNT -lt $BEST_GAME || -z $BEST_GAME ]]; then
+        NEW_BEST_GAME=$GUESS_COUNT
+      else
+        NEW_BEST_GAME=$BEST_GAME
+      fi
+
+      UPDATE_RESULT=$($PSQL "UPDATE users SET games_played=$NEW_GAMES_PLAYED, best_game=$NEW_BEST_GAME WHERE username='$USERNAME'")
+    fi
+  elif [[ $GUESS -gt $SECRET_NUMBER ]]; then
+    echo "It's lower than that, guess again:"
+  else
+    echo "It's higher than that, guess again:"
+  fi
+done
